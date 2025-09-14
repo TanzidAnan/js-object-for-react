e21c9674b19443eed837363ab57cdb5cfeee5295
TanzidAnan <tanzid.anan125@gmail.com>
2025-09-14 22:31:42 +0600

    number

---
+++ fundamentals.js
@@ -0,0 +1,29 @@
+// 1.How to declare a variable using let and onst
+
+const fastName ='Anan';
+let season ='rainy';
+season='winter';
+
+if(fastName === 'anan' || season ==='rainy'){
+
+}
+else{
+
+}
+
+// 3.array
+// index
+// length
+// push
+// pop
+const numbers =[12,36,54,12,96,4,53,64,63,45];
+
+// Loop
+
+// for loop 
+// in loop
+
+for(let i =0; i<numbers.length;i++){
+    const number =numbers[i];
+    console.log(number)
+}
