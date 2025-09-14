24ecb20ca136d87bb6535cb7edd19219fd5ffb63
TanzidAnan <tanzid.anan125@gmail.com>
2025-09-14 22:45:29 +0600

    arrow functin

--- es6.js
+++ es6.js
@@ -1,8 +1,8 @@
-const numbers =[45,36,4,6556,9,44,1,96,];
-const student ={
-    name:'tanzid Anan',
+const numbers = [45, 36, 4, 6556, 9, 44, 1, 96,];
+const student = {
+    name: 'tanzid Anan',
     age: 32,
-    mivies:['king Khan','Dhakar mastan'],
+    mivies: ['king Khan', 'Dhakar mastan'],
 
 }
 
@@ -11,5 +11,15 @@ const student ={
 
 
 
-const about =`my name ${student.name} my age ${student.age}`;
+const about = `my name ${student.name} my age ${student.age} has numbers ${numbers[2]}  and movise ${student.mivies[0]}`;
 console.log(about)
+
+
+// arrow function
+
+const getFiftyFive = () => 55;
+
+const addSixtyFive = (num) => num + 65;
+
+const isEvent = (x) => x % 2 == 0;
+
