0a87dbb5a8267412c4ac6579ba6fc2613034edf6
TanzidAnan <tanzid.anan125@gmail.com>
2025-09-14 22:41:29 +0600

    templated

---
+++ es6.js
@@ -0,0 +1,15 @@
+const numbers =[45,36,4,6556,9,44,1,96,];
+const student ={
+    name:'tanzid Anan',
+    age: 32,
+    mivies:['king Khan','Dhakar mastan'],
+
+}
+
+
+
+
+
+
+const about =`my name ${student.name} my age ${student.age}`;
+console.log(about)
--- fundamentals.js
+++ fundamentals.js
@@ -1,13 +1,13 @@
 // 1.How to declare a variable using let and onst
 
-const fastName ='Anan';
-let season ='rainy';
-season='winter';
+const fastName = 'Anan';
+let season = 'rainy';
+season = 'winter';
 
-if(fastName === 'anan' || season ==='rainy'){
+if (fastName === 'anan' || season === 'rainy') {
 
 }
-else{
+else {
 
 }
 
@@ -16,14 +16,23 @@ else{
 // length
 // push
 // pop
-const numbers =[12,36,54,12,96,4,53,64,63,45];
+const numbers = [12, 36, 54, 12, 96, 4, 53, 64, 63, 45];
 
 // Loop
 
 // for loop 
 // in loop
 
-for(let i =0; i<numbers.length;i++){
-    const number =numbers[i];
+for (let i = 0; i < numbers.length; i++) {
+    const number = numbers[i];
     console.log(number)
 }
+
+// function
+
+function multiple(num1, mum2) {
+    const result = num1 * mum2;
+    return result
+}
+
+console.log(multiple(25,36))
