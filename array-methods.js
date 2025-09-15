69d38477247028ee343d1f5d2b9565d99f35c81c
TanzidAnan <tanzid.anan125@gmail.com>
2025-09-15 11:22:36 +0600

    destructuring

--- array-methods.js
+++ array-methods.js
@@ -12,8 +12,23 @@ const products =[
     {name:'laptop',price:3200,brand:'lenovo',color:'black'},
 ];
 
+// Map
+// forEach
+// filter
+// find
 
 
 
 const brands =products.map(p =>p.brand);
-console.log(brands)
+// console.log(brands);
+
+// products.forEach(p =>console.log(p))
+products.forEach(p =>{
+    // console.log(p)
+})
+
+// includes
+
+// filter
+const cheap =products.filter(p =>p.price<3000);
+console.log(cheap)
---
+++ destructuring.js
@@ -0,0 +1,4 @@
+const numbers = [54, 36, 5, 25, 3, 6, 1, 4, 63, 4, 13, 85];
+
+const [x, y] = numbers;
+console.log(x,y)
